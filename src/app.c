873ed7c8a39fcf5348f656c07c7eb843265cb0b7
aloima <acar.aloima@gmail.com>
2024-07-21 17:41:11 +0300

    add flagging

--- src/app.c
+++ src/app.c
@@ -4,7 +4,9 @@
 #include <stdint.h>
 #include <string.h>
 
-void redraw_board(struct Tiles tiles, uint32_t y, uint32_t x) {
+void redraw_board(struct Tiles tiles, uint32_t y, uint32_t x, uint32_t remaining_flags) {
+  mvprintw(y - 1, x, "Remaining flags: %-3u", remaining_flags);
+
   for (uint32_t i = 0; i < tiles.size; ++i) {
     const uint32_t mx = i % 9;
     const uint32_t my = i / 9;
@@ -33,6 +35,8 @@ void generate_board(struct Options options, uint32_t y, uint32_t x) {
     l2 = line with spaces
     ll = last line
   */
+  mvprintw(y - 1, x, "Remaining flags: %-3u", options.mine_count);
+
   const uint32_t ml4 = options.minefield_len * 4;
   chtype fl[ml4 + 2], ll[ml4 + 2];
   chtype l1[ml4 + 2], l2[ml4 + 2];
@@ -106,6 +110,7 @@ void init_game() {
   keypad(stdscr, true);
 
   struct Tiles tiles = generate_empty_tiles(options.minefield_len);
+  uint32_t remaining_flags = options.mine_count;
 
   while (true) {
     const int32_t c = getch();
@@ -124,7 +129,7 @@ void init_game() {
         generate_board(options, start_y, start_x);
 
         if (placed_mines) {
-          redraw_board(tiles, start_y, start_x);
+          redraw_board(tiles, start_y, start_x, remaining_flags);
         }
 
         break;
@@ -133,25 +138,44 @@ void init_game() {
         MEVENT event;
 
         if (getmouse(&event) == OK) {
+          const uint32_t _y = event.y - start_y;
+          const uint32_t _x = event.x - start_x;
+
+          const pos_t click_at = {
+            .x = (_x % 4 == 0) ? 0 : ((_x / 4) + 1),
+            .y = (_y % 2 == 0) ? 0 : ((_y / 2) + 1)
+          };
+
           switch (event.bstate) {
-            case BUTTON1_CLICKED: {
-              const uint32_t _y = event.y - start_y;
-              const uint32_t _x = event.x - start_x;
+            case BUTTON1_CLICKED:
+              if (!placed_mines) {
+                struct Offset offset = {
+                  .bottom = 1,
+                  .top = 1,
+                  .left = 1,
+                  .right = 1
+                };
 
-              const pos_t click_at = {
-                .y = (_y % 2 == 0) ? 0 : ((_y / 2) + 1),
-                .x = (_x % 4 == 0) ? 0 : ((_x / 4) + 1)
-              };
+                placed_mines = place_mines(tiles, options.mine_count, click_at, offset);
+                redraw_board(tiles, start_y, start_x, remaining_flags);
+              }
 
-              struct Offset offset = {
-                .bottom = 1,
-                .top = 1,
-                .left = 1,
-                .right = 1
-              };
+              break;
 
-              placed_mines = place_mines(tiles, options.mine_count, click_at, offset);
-              redraw_board(tiles, start_y, start_x);
+            case BUTTON3_CLICKED: {
+              if (click_at.x != 0 && click_at.y != 0) {
+                tile_t *tile = get_tile(tiles, click_at.x, click_at.y);
+
+                if (tile->flagged) {
+                  tile->flagged = false;
+                  ++remaining_flags;
+                  redraw_board(tiles, start_y, start_x, remaining_flags);
+                } else if (!tile->opened && remaining_flags != 0) {
+                  tile->flagged = true;
+                  --remaining_flags;
+                  redraw_board(tiles, start_y, start_x, remaining_flags);
+                }
+              }
 
               break;
             }
